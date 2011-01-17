415efeca9d4f09d9850b6da5b0d954b0432f7b35
Paul Butcher <paul@paulbutcher.com>
2011-01-17 01:30:38 +0000

    Ripples working, but not very well

--- src/RippleView.scala
+++ src/RippleView.scala
@@ -2,22 +2,47 @@ package com.paulbutcher.scalakey
 
 import android.content.Context
 import android.graphics.{BitmapFactory, Canvas}
-import android.view.{Display, View, WindowManager}
+import android.view.{Display, MotionEvent, View, WindowManager}
 
 class RippleView(context: Context) extends View(context) with Logger {
 
   val bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.water)
   val (width, height) = getSize
-  val mesh = new Mesh(width, height)
+  
+  val unwarpedMesh = new Mesh(width, height)
   
   override def onDraw(canvas: Canvas) {
     d("onDraw")
+    val warpedMesh = new Mesh(width, height, rippleX, rippleY, rippleStartTime)
+    val mesh = if (warpedMesh.done) unwarpedMesh else warpedMesh
     canvas.drawBitmapMesh(bitmap, mesh.width, mesh.height, mesh.vertices, 0, null, 0, null)
+    if (!warpedMesh.done) invalidate
   }
   
   override def onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
     d("onMeasure")
     setMeasuredDimension(width, height)
+  }
+  
+  override def onTouchEvent(event: MotionEvent): Boolean = {
+    d("onTouchEvent")
+    if (event.getAction == MotionEvent.ACTION_DOWN) {
+      val (x, y) = (event.getX, event.getY)
+      startRipple(x, y)
+    }
+    true
+  }
+  
+  private var rippleX: Float = _
+  private var rippleY: Float = _
+  private var rippleStartTime: Long = _
+  
+  private def startRipple(x: Float, y: Float) {
+    d("startRipple: "+ x +", "+ y)
+    rippleX = x
+    rippleY = y
+    rippleStartTime = System.currentTimeMillis
+    invalidate
   }
   
   private def getSize(): (Int, Int) = {
